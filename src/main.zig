a9d51d02ddb24649036bb27ff3f8528fd6d5895b
jfsog <Felipe.sg@protonmail.com>
2025-07-25 06:19:31 -0300

    change dimension of cube

--- src/main.zig
+++ src/main.zig
@@ -5,15 +5,15 @@ const N = 3;
 
 pub fn main() !void {
     const alloc = std.heap.c_allocator;
-    const screenWidth: i32 = 640;
-    const screenHeight: i32 = 640;
+    const screenWidth: i32 = 1024;
+    const screenHeight: i32 = 1024;
 
     rl.initWindow(screenWidth, screenHeight, "rubik - Zig + raylib");
     defer rl.closeWindow(); // Close window and OpenGL context
 
     rl.setTargetFPS(30); // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
-    var cube = try rc.Init(alloc, 3);
+    var cube = try rc.Init(alloc, 5);
     // defer cube.deinit(); // Deinitialize the cube when done
     while (!rl.windowShouldClose()) { // Detect window close button or ESC key
         rl.beginDrawing();
